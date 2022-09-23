93e96b978550e9bd25fb4235d0388d35ac4f7cd2
Vladislav Burhovetskiy <97479316+Vladislav-Burhovetskiy@users.noreply.github.com>
2022-09-23 22:03:14 +0300

    Delete about-us.js

--- about-us.js
+++
@@ -1,6 +0,0 @@
-(function() { const clockContainer = document.querySelector('.clock');
-function updateClock () {
-clockContainer.innerText = new Date().toLocaleTimeString();
-}
-setInterval(updateClock, 1000);
-})();
