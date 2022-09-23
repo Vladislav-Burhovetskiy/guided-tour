8f22713a78cfa8ce16c113ed669346f6ac7eef07
Vladislav Burhovetskiy <97479316+Vladislav-Burhovetskiy@users.noreply.github.com>
2022-09-23 22:03:01 +0300

    Delete about-norway.js

--- about-norway.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    const clockContainer = document.querySelector ('.about__norway-description-clock');
-    function undateClock() {
-    clockContainer.innerText = new Date().toLocaleDateString();  
-    }
-    setInterval(undateClock, 1000);
-}) ();
-
-
